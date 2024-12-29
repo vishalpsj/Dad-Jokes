359a60929af03f4f73255ceddd68a1aa564d10fe
Vishal.P <73173958+vishalpsj@users.noreply.github.com>
2024-12-29 17:22:00 +0530

    little modifications

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 let btn = document.getElementById('btn')
-        let jokeBox = document.querySelector('.jokeBox')
-        let jokeURL = "https://icanhazdadjoke.com/"
+let jokeBox = document.querySelector('.jokeBox')
+let jokeURL = "https://icanhazdadjoke.com/"
 
 
 
-        const fetchData = () => {
-            fetch(jokeURL, {
-                headers : {
-                    Accept : "application/json"
-                }
-            })
-            .then((res) => {
-                // console.log(res);
-                return res.json();
-            })
-            .then((data) => {
-                // console.log(data.joke);
-                jokeBox.textContent = `${data.joke}`
-                
-            })
-            .catch((error) => {
-                console.log(error);
-                
-            })
+const fetchData = () => {
+    fetch(jokeURL, {
+        headers: {
+            Accept: "application/json"
         }
+    })
+        .then((res) => {
+            // console.log(res);
+            return res.json();
+        })
+        .then((data) => {
+            // console.log(data.joke);
+            jokeBox.textContent = `${data.joke}`
 
-        fetchData()
+        })
+        .catch((error) => {
+            console.log(error);
 
-        btn.addEventListener('click', fetchData)
+        })
+}
+
+fetchData()
+
+btn.addEventListener('click', fetchData)
