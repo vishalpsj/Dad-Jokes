d201479f1887bd946d87019a7c2991d7374a8745
Vishal.P <73173958+vishalpsj@users.noreply.github.com>
2024-12-29 16:23:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById('btn')
+        let jokeBox = document.querySelector('.jokeBox')
+        let jokeURL = "https://icanhazdadjoke.com/"
+
+
+
+        const fetchData = () => {
+            fetch(jokeURL, {
+                headers : {
+                    Accept : "application/json"
+                }
+            })
+            .then((res) => {
+                // console.log(res);
+                return res.json();
+            })
+            .then((data) => {
+                // console.log(data.joke);
+                jokeBox.textContent = `${data.joke}`
+                
+            })
+            .catch((error) => {
+                console.log(error);
+                
+            })
+        }
+
+        fetchData()
+
+        btn.addEventListener('click', fetchData)
